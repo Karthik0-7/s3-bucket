25fe2617f27240546f9c9d4e2b324b57453b687a
Karthik <narsapurkarthik@gmail.com>
2025-04-15 20:59:35 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "bucket_name" {
+  type        = string
+  description = "The name of the S3 bucket"
+}
+
+variable "region" {
+  type        = string
+  description = "AWS region"
+}
+
+variable "versioning" {
+  type        = bool
+  description = "Enable versioning"
+}
