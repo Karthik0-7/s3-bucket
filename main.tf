17fa52386b0713cfef940db14d0330bcd16618ab
Karthik <narsapurkarthik@gmail.com>
2025-04-16 13:10:31 +0530

    added versioning to main.tf

--- main.tf
+++ main.tf
@@ -20,3 +20,12 @@ resource "aws_s3_bucket" "bucket" {
     CreatedBy = "Backstage"
   }
 }
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  count  = var.versioning ? 1 : 0
+  bucket = aws_s3_bucket.bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
